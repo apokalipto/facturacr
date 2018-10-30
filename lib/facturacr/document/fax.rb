ac276977be5cb54ff8f52545b5d47cefb5f7417a
DanielES <daniel.elizondo@delaguayaba.com>
2018-10-30 16:43:41 -0600

    Prevent duplicate validatios for the phone and fax class

--- lib/facturacr/document/fax.rb
+++ lib/facturacr/document/fax.rb
@@ -7,18 +7,14 @@ module FE
   class Document
     class Fax < PhoneType
       include ActiveModel::Validations
-      
+
       attr_accessor :country_code, :number
-      
-      validates :country_code, presence: true, length: { maximum: 3 }
-      validates :number, presence: true, length: {maximum: 20}
-      
-      
-      def initialize(args={})
+
+      def initialize(args = {})
         super('Fax', args[:country_code], args[:number])
       end
-      
+
     end
-    
+
   end
 end
