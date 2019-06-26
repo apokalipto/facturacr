4ac41c0ed4ca0f1546761ad13c421dd6188bb1ac
Josef Sauter <Josef.Sauter@gmail.com>
2019-06-26 15:57:17 -0600

    fix typos

--- lib/facturacr/reception_message.rb
+++ lib/facturacr/reception_message.rb
@@ -50,7 +50,7 @@ module FE
     validates :receiver_id_type, presence: true
     validates :economic_activity, presence: true, if: ->{ version_43? && tax_condition != "05"}
     validates :creditable_tax, presence: true, if: -> { version_43? && tax_condition != "05" && (creditable_tax.present? && tax != creditable_tax) }
-    validates :applicable_expense, presence: true, if: -> { version_43? && tax_condition != "05" && (applicable_tax.present? && tax != creditable_tax) }
+    validates :applicable_expense, presence: true, if: -> { version_43? && tax_condition != "05" && (applicable_expense.present? && tax != creditable_tax) }
 
     def initialize(args = {})
       @version = args[:version]
