353b1b0b9757f6a8a70757a1aeb772b4323d0100
antimasi <camiranda1995@gmail.com>
2025-04-01 14:09:22 -0600

    exoneration changed

--- lib/facturacr/document/exoneration.rb
+++ lib/facturacr/document/exoneration.rb
@@ -14,7 +14,7 @@ module FE
         "07" => "Transitorio XVII",
         "99" => "Otros"
       }.freeze
-      attr_accessor :document_type, :document_number, :institution, :date, :total_tax, :percentage, :net_total
+      attr_accessor :document_type, :document_number, :institution, :date, :total_tax, :percentage, :net_total,:document_type_other,:section,:subsection,:institution_other
 
       validates :document_type, presence: true, inclusion: DOCUMENT_TYPES.keys
       validates :document_number, presence: true, length: { maximum: 40 }
@@ -22,7 +22,7 @@ module FE
       validates :date, presence: true
       validates :total_tax,presence: true
       validates :percentage, presence: true, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 100, only_integer: true}
-      
+
 
       def initialize(args={})
         @document_type = args[:document_type]
@@ -31,6 +31,10 @@ module FE
         @date = args[:date]
         @total_tax = args[:total_tax]
         @percentage = args[:percentage]
+        @document_type_other = args[:document_type_other]
+        @section = args[:section]
+        @subsection = args[:subsection]
+        @institution_other = args[:institution_other]
       end
 
       def build_xml(node, document)
@@ -39,12 +43,19 @@ module FE
         node = Nokogiri::XML::Builder.new if node.nil?
 
         node.Exoneracion do |xml|
-          xml.TipoDocumento @document_type
+          xml.TipoDocumento @document_type if document.version_43?
+          xml.TipoDocumentoEX1 @document_type if document.version_44?
           xml.NumeroDocumento @document_number
+          xml.Articulo @section if document.version_44?
+          xml.Inciso @subsection if document.version_44?
           xml.NombreInstitucion @institution
-          xml.FechaEmision @date.xmlschema
-          xml.PorcentajeExoneracion @percentage
+          xml.NombreInstitucionOtros @institution_other
+          xml.FechaEmision @date.xmlschema if document.version_43?
+          xml.FechaEmisionEX @date.xmlschema if document.version_44?
+          xml.PorcentajeExoneracion @percentage if document.version_43?
+          xml.TarifaExonerada @percentage if document.version_44?
           xml.MontoExoneracion @total_tax
+
         end
       end
 
