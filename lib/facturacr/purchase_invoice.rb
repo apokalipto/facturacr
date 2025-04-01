1a24b21438a2b56702fa7c7079cc4c62d933472e
antimasi <camiranda1995@gmail.com>
2025-04-01 14:50:40 -0600

    4.4 fields added to purchase_invoice

--- lib/facturacr/purchase_invoice.rb
+++ lib/facturacr/purchase_invoice.rb
@@ -3,10 +3,21 @@ require 'facturacr/document'
 module FE
 
   class PurchaseInvoice < Document
+    NAMESPACES ={
+      "4.3" => {
+        "xmlns:xsi"=>"http://www.w3.org/2001/XMLSchema-instance",
+        "xmlns:xsd"=>"http://www.w3.org/2001/XMLSchema",
+        "xmlns"=>"https://cdn.comprobanteselectronicos.go.cr/xml-schemas/v4.3/facturaElectronicaCompra"#,
+      },
+      "4.4" => {
+        "xmlns:xsi"=>"http://www.w3.org/2001/XMLSchema-instance",
+        "xmlns:xsd"=>"http://www.w3.org/2001/XMLSchema",
+        "xmlns"=>"https://cdn.comprobanteselectronicos.go.cr/xml-schemas/v4.4/facturaElectronicaCompra"#,
+      }}
     DOCUMENT_TYPE = "08"
-    
+
     validates :receiver, presence: true, if: -> { version.eql?("4.3") }
-    
+
     def initialize(args={})
       @version = args[:version]
       @economic_activity = args[:economic_activity]
@@ -24,12 +35,11 @@ module FE
       @document_situation = args[:document_situation]
       @other_charges = args[:other_charges]
       @references = args[:references] || []
-      @namespaces = {
-        "xmlns:xsi"=>"http://www.w3.org/2001/XMLSchema-instance",
-        "xmlns:xsd"=>"http://www.w3.org/2001/XMLSchema",
-        "xmlns"=>"https://cdn.comprobanteselectronicos.go.cr/xml-schemas/v4.3/facturaElectronicaCompra"#,
-      }
+      @namespaces = NAMESPACES[@version]
       @others = args[:others] || []
+      @software_supplier = args[:software_supplier]
+      @receiver_economic_activity = args[:receiver_economic_activity]
+      @other_condition = args[:other_condition]
     end
 
     def document_tag
