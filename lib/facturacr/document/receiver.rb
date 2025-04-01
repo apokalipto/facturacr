e76d15f034aaa6a50a1ea3f7c9786919a189e990
antimasi <camiranda1995@gmail.com>
2025-04-01 14:11:45 -0600

    4.4 fields added to receiver

--- lib/facturacr/document/receiver.rb
+++ lib/facturacr/document/receiver.rb
@@ -11,16 +11,16 @@ module FE
         attr_accessor :name, :identification_document,:foreign_id_number, :comercial_name, :location, :phone, :fax, :email,:other_foreign_signs, :document_type
 
         validates :name, presence: true
-        
+
         validates :identification_document, presence: true, if: -> {document_type.eql?("01") || document_type.eql?("08")}
         validates :foreign_id_number, length: { maximum: 20 }, if: -> {document_type.eql?("01") || document_type.eql?("08")}
         validates :other_foreign_signs,length: { maximum: 300 }
         validates :email, length: {maximum: 160}, format:{with: /\s*\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*\s*/}, if: ->{ email.present? }
-        
+
 
         validates :name, length: { maximum: 80}, if: ->{ document.version_42? }
         validates :comercial_name, length: { maximum: 80 }, if: ->{ document.version_42? }
-    
+
 
         validates :name, length: { maximum: 100}, if: ->{ document.version_43? }
         validates :comercial_name, length: { maximum: 100 }, if: ->{ document.version_43? }
@@ -44,12 +44,12 @@ module FE
           @document = document
           @document_type = document.document_type
           raise FE::Error.new("receiver invalid",class: self.class, messages: errors.messages) unless valid?
-          
+
           node = Nokogiri::XML::Builder.new if node.nil?
           node.Receptor do |xml|
             xml.Nombre @name
             @identification_document.build_xml(xml,@document) if @identification_document.present?
-            xml.IdentificacionExtranjero foreign_id_number if @foreign_id_number.present?
+            xml.IdentificacionExtranjero foreign_id_number if @foreign_id_number.present? && @document.version_43?
             xml.NombreComercial @comercial_name if @comercial_name.present?
             @location.build_xml(xml,@document) if @location.present?
             @phone.build_xml(xml, @document) if @phone.present?
