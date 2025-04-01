7b7d691760bd9e975853d9cc98c59e698739e884
antimasi <camiranda1995@gmail.com>
2025-04-01 14:10:59 -0600

    4.4 fields added to issuer

--- lib/facturacr/document/issuer.rb
+++ lib/facturacr/document/issuer.rb
@@ -5,20 +5,21 @@ module FE
       class Issuer < Element
         include ActiveModel::Validations
 
-        attr_accessor :name, :identification_document, :comercial_name, :location, :phone, :fax, :email
+        attr_accessor :name, :identification_document, :comercial_name, :location, :phone, :fax, :email, :fiscal_registry_8707
 
         validates :name, presence: true
         validates :identification_document, presence: true
         validates :location, presence: true
         validates :email, presence: true,length: {maximum: 160}, format:{with: /\s*\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*\s*/}
-        
+
+
         validates :name, length: { maximum: 80}, if: ->{ document.version_42? }
         validates :comercial_name, length: { maximum: 80 }, if: ->{ document.version_42? }
 
         validates :name, length: { maximum: 100}, if: ->{ document.version_43? }
         validates :comercial_name, length: { maximum: 100 }, if: ->{ document.version_43? }
 
-        
+
 
         def initialize(args={})
           @name = args[:name]
@@ -28,6 +29,7 @@ module FE
           @phone = args[:phone]
           @fax = args[:fax]
           @email = args[:email]
+          @fiscal_registry_8707 = args[:fiscal_registry_8707]
         end
 
         def build_xml(node, document)
@@ -39,10 +41,18 @@ module FE
             xml.Nombre @name
             identification_document.build_xml(xml,document)
             xml.NombreComercial @comercial_name if @comercial_name
+            xml.Registrofiscal8707 @fiscal_registry_8707 if @fiscal_registry_8707
             location.build_xml(xml, document)
             phone.build_xml(xml, document) if phone.present?
             fax.build_xml(xml, document) if fax.present?
-            xml.CorreoElectronico @email
+            if document.version_42? || document.version_43?
+              xml.CorreoElectronico @email
+            elsif document.version_44?
+              @email = [@email] if !@email.is_a?(Array)
+              @email.each do |e|
+                xml.CorreoElectronico e
+              end
+            end
           end
         end
 
