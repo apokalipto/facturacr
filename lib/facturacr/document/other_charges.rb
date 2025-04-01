700a430d745df19822bc22fe45e41d3623faf4fc
antimasi <camiranda1995@gmail.com>
2025-04-01 14:38:46 -0600

    4.4 fields added to other_charges

--- lib/facturacr/document/other_charges.rb
+++ lib/facturacr/document/other_charges.rb
@@ -16,7 +16,7 @@ module FE
             "99" => "Otros Cargos"
         }.freeze
 
-        attr_accessor :document_type, :collector_id_number, :collector_name, :detail, :percentage, :total_charge
+        attr_accessor :document_type, :collector_id_number, :collector_name, :detail, :percentage, :total_charge, :document_type_other
 
         validates :document_type, presence: true, inclusion: OTHER_DOCUMENT_TYPES.keys
         validates :collector_id_number, presence: false, if: ->{ document_type.eql?("04") }
@@ -31,6 +31,7 @@ module FE
           @detail = args[:detail]
           @percentage =args[:percentage]
           @total_charge = args[:total_charge]
+          @document_type_other = args[:document_type_other]
         end
 
         def build_xml(node, document)
@@ -39,11 +40,15 @@ module FE
           node = Nokogiri::XML::Builder.new if node.nil?
 
           node.OtrosCargos do |xml|
-              xml.TipoDocumento @document_type
-              xml.NumeroIdentidadTercero @third_id_number if @third_id_number.present?
+              xml.TipoDocumento @document_type if document.version_43?
+              xml.TipoDocumentoOC @document_type if document.version_44?
+              xml.TipoDocumentoOTROS @document_type_other if document.version_44?
+              xml.NumeroIdentidadTercero @third_id_number if document.version_43? && @third_id_number.present?
+              xml.NumeroIdentidadTercero @third_id_number if document.version_43? && @third_id_number.present?
               xml.NombreTercero @third_name if @third_name.present?
               xml.Detalle @detail
-              xml.Porcentaje @percentage if @percentage.present?
+              xml.Porcentaje @percentage if @percentage.present? &&  document.version_43?
+              xml.PorcentajeOC @percentage if @percentage.present? &&  document.version_44?
               xml.MontoCargo @total_charge
 
           end
